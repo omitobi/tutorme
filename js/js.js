c08cd2fec33918763e0ad272f4aa82cccacd9222
Omisakin Oluwatobi Samuel <oluwatob@ut.ee>
2017-03-12 02:48:23 +0200

    Redesign search page

--- js/js.js
+++ js/js.js
@@ -70,3 +70,22 @@ function shakeModal(){
 }
 
 
+/**
+ * Flipcard Js
+ */
+
+$().ready(function(){
+    $('[rel="tooltip"]').tooltip();
+
+});
+
+function rotateCard(btn){
+    var $card = $(btn).closest('.card-container');
+    console.log($card);
+    if($card.hasClass('hover')){
+        $card.removeClass('hover');
+    } else {
+        $card.addClass('hover');
+    }
+}
+
